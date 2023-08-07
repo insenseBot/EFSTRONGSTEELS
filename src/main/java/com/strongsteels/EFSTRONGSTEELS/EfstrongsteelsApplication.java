c74bb76e43c15e10ce8b4feb8c81837d0a25358d
insenseBot <129139229+insenseBot@users.noreply.github.com>
2023-08-07 23:38:44 +0000

    Comienzo (limpio)

---
+++ src/main/java/com/strongsteels/EFSTRONGSTEELS/EfstrongsteelsApplication.java
@@ -0,0 +1,13 @@
+package com.strongsteels.EFSTRONGSTEELS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfstrongsteelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfstrongsteelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/strongsteels/EFSTRONGSTEELS/EfstrongsteelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strongsteels.EFSTRONGSTEELS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfstrongsteelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
